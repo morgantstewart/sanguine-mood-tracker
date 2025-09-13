0416e2dffb53489910557f3568e014a7dd04c4a2
morgantstewart <morgan@paysigil.com>
2025-09-13 00:22:30 -0400

    Included static files in templates

--- sanguine/settings.py
+++ sanguine/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'sanguine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'sanguine' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- sanguine/views.py
+++ sanguine/views.py
@@ -7,8 +7,7 @@ from django.http import HttpResponse
 
 # Define the home view function
 def home(request):
-    # Send a simple HTML response
-    return HttpResponse('<h1>Hello from Sanguine</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
